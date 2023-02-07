ad38cac19bb48957342b2430de83e9f1f6c617bd
PierreLuc_JolyBoulanger <pierre_luc_95@hotmail.com>
2023-02-07 15:30:40 -0500

    Added another constructor with length parameters

--- demo/Octogone.cs
+++ demo/Octogone.cs
@@ -2,8 +2,10 @@ public class Octogone {
     private int _longueur;
     private readonly int _nombreCote = 8;
 
-    public Octogone(){
-
+    public Octogone() {
     }
-    
+
+    public Octogone(int longueur) {
+        this.longueur = _longueur;
+    }
 }
